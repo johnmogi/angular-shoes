dac3e3cf4fb6d15205e9b99b4e7d4ccc89893b77
john <john@localhost.localdomain>
2020-03-11 05:20:10 +0200

    start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { LayoutComponent } from "./components/layout/layout.component";
+import { HeaderComponent } from "./components/header/header.component";
+import { FooterComponent } from "./components/footer/footer.component";
+import { MenuComponent } from "./components/menu/menu.component";
+import { HomeComponent } from "./components/home/home.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    LayoutComponent,
+    HeaderComponent,
+    FooterComponent,
+    MenuComponent,
+    HomeComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [LayoutComponent]
 })
-export class AppModule { }
+export class AppModule {}
